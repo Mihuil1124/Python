8487fdbf227c239d8ee4a236f3477e9a9091516d
Mihuil1124 <40023826+Mihuil1124@users.noreply.github.com>
2019-10-28 23:15:51 +0300

    Update ReadURLWriteInFile.py
    Add 'encoding="utf-8" '

--- ReadURLWriteInFile.py
+++ ReadURLWriteInFile.py
@@ -2,9 +2,9 @@ import urllib.request
 tab = input('Write URL - ')
 page = urllib.request.urlopen(tab)
 FuncRead = page.read()
-text = FuncRead.decode("utf8")
+text = FuncRead.decode("utf-8")
 page.close()
 
-f = open('Site.txt', 'w')
+f = open('Site.txt', 'w', encoding="utf-8")
 f.writelines(text)
 f.close()
