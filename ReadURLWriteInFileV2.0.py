c71b29d53be42a0dd61da6322f0810ee847af559
Mihuil1124 <40023826+Mihuil1124@users.noreply.github.com>
2019-11-08 20:58:38 +0300

    Delete ReadURLWriteInFileV2.0.py

--- ReadURLWriteInFileV2.0.py
+++
@@ -1,9 +0,0 @@
-import urllib.request
-tab = input('Write URL - ')
-page = urllib.request.urlopen(tab)
-text=str(page.read().decode('utf-8'))
-page.close()
-
-f = open('Site.txt', 'w', encoding="utf-8")
-f.writelines(text)
-f.close()
